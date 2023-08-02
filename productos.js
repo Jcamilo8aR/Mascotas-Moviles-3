e81658feed3d63b58231b9518e5c2b1042477994
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-08-02 09:04:39 -0500

    Inicio

---
+++ productos.js
@@ -0,0 +1,23 @@
+let  productos = [
+    {
+        "id": 1,
+        "nombre": "Alimento para perro Proplan (3,5kg)",
+        "precio": 181600,
+        "imagen": "https://kanu.vtexassets.com/arquivos/ids/166796-800-auto?v=638243455124630000&width=800&height=auto&aspect=true",
+        "disponible": "si"
+    },
+    {
+        "id": 2,
+        "nombre": "Alimento para perro Proplan (3,5kg)",
+        "precio": 181600,
+        "imagen": "https://kanu.vtexassets.com/arquivos/ids/166796-800-auto?v=638243455124630000&width=800&height=auto&aspect=true",
+        "disponible": "si"
+    },
+    {
+        "id": 3,
+        "nombre": "Alimento para perro Proplan (3,5kg)",
+        "precio": 181600,
+        "imagen": "https://kanu.vtexassets.com/arquivos/ids/166796-800-auto?v=638243455124630000&width=800&height=auto&aspect=true",
+        "disponible": "si"
+    },
+]
